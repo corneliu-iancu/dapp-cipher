789757de369cabf6e88fc5c62d6c9926bb7538de
Corneliu Iancu <corneliu.iancu27@gmail.com>
2022-04-13 10:54:23 +0000

    update MintToolbar component

--- src/pages/Dashboard/MintToolbar/index.tsx
+++ src/pages/Dashboard/MintToolbar/index.tsx
@@ -3,14 +3,14 @@ import EsdtBalance from 'common/EsdtBalance';
 
 const MintToolbar = ({ egldBalance, esdtBalance }: any) => {
   return (
-    <div className='col-12 mb-4 bg-white py-4'>
+    <div className='col-12 mb-4 bg-white py-4 border-top border-bottom border-dark'>
       <div className='row'>
-        <div className='col-3'></div>
-        <div className='col-3'></div>
-        <div className='col-3'>
+        <div className='col-12 col-sm-6 col-md-3'></div>
+        <div className='col-12 col-sm-6 col-md-3'></div>
+        <div className='col-12 col-sm-6 col-md-3 mb-2'>
           <EsdtBalance balance={egldBalance} />
         </div>
-        <div className='col-3'>
+        <div className='col-12 col-sm-6 col-md-3 mb-2'>
           <EsdtBalance balance={esdtBalance} currency='GELD' decimals={0} />
         </div>
       </div>
